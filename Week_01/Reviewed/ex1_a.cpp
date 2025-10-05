8e42af8bbcaef661306999bb83a6913b9df5843d
Pham Phuoc Minh Hieu <hieuphamminh976@gmail.com>
2025-10-05 19:10:32 +0700

    Update ex1_a

---
+++ Week_01/Reviewed/ex1_a.cpp
@@ -0,0 +1,17 @@
+// ex1-a
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cout << "x: ";
+    cin >> x;
+    
+    if (x + 1 == 2){
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+    
+    return 0;
+}
